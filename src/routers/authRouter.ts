4f7f8c330e7a6daa9e21832e1d38b99369dcd41d
RodolphoBuettel <Rodolpho05buettel@gmail.com>
2023-03-29 21:33:28 -0300

    fix: bugs on authrouter

--- src/routers/authRouter.ts
+++ src/routers/authRouter.ts
@@ -1,11 +1,11 @@
 import { SignIn, SignUp } from "../controllers/authController";
 import { Router } from "express";
-import { ValidateSignUpBody } from "../middlewares/modelsValidations";
+import { ValidateSignInBody, ValidateSignUpBody } from "../middlewares/modelsValidations";
 
 const authRouter = Router();
 
 authRouter
     .post("/sign-up", ValidateSignUpBody, SignUp)
-    .post("/sign-in", ValidateSignUpBody, SignIn);
+    .post("/sign-in", ValidateSignInBody, SignIn);
 
 export default authRouter;
